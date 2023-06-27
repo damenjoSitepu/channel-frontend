f21e8c0f9074359e528d74b7ebe149a58d8367a9
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-06-27 10:02:40 +0700

    Update: Finish modal configuration for list status functionality

--- src/app/shared/modals/create-list-status/create-list-status.component.ts
+++ src/app/shared/modals/create-list-status/create-list-status.component.ts
@@ -2,18 +2,22 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { NgClickOutsideDirective } from 'ng-click-outside2';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-create-list-status',
   standalone: true,
-  imports: [CommonModule, DragDropModule, ComponentsModule],
+  imports: [CommonModule, DragDropModule, ComponentsModule, NgClickOutsideDirective],
   templateUrl: './create-list-status.component.html',
   styleUrls: ['./create-list-status.component.scss']
 })
 export class CreateListStatusComponent {
+
     constructor(
         private _el: ElementRef,
-        private _renderer: Renderer2
+        private _renderer: Renderer2,
+        private _modalService: ModalService
     ) {}
 
     resizeTextArea(id: string): void {
@@ -57,5 +61,16 @@ export class CreateListStatusComponent {
         this._renderer.setStyle(labelEl, 'fontSize', '13px');
         this._renderer.setStyle(labelEl, 'color', '#fff');
         el.blur();
+    }
+
+    clickOutsideOperationsOptions(e: Event): void {
+        e.stopPropagation();
+        if (!(e.target as HTMLElement).classList.contains('channel__list_status_main_options_create')) {
+            this._modalService.dismiss();
+        }
+    }
+
+    submit(): void {
+        this._modalService.dismiss();
     }
 }
